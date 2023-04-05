32d24dfaba5b36f14aeb1af9515648a46b8026aa
ZEAKING X DXT <129081220+Zeadxt@users.noreply.github.com>
2023-04-05 21:10:38 +0700

    Update and rename trial41.sh to trial4.sh

--- trial4.sh
+++ trial4.sh
@@ -79,9 +79,9 @@ wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | a
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;41;36m            SSH Account            \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-Login=
-Pass=
-masaaktif=
+Login=Trial-SSH-`</dev/urandom tr -dc 0-9 | head -c7`-CAPEK
+Pass=1
+masaaktif=1
 
 IP=$(curl -sS ifconfig.me);
 ossl=`cat /root/log-install.txt | grep -w "OpenVPN" | cut -f2 -d: | awk '{print $6}'`
